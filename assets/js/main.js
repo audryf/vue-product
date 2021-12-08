5122ee4ac50ea892e5138a6673c7f0c3db94897d
audryf <84256033+audryf@users.noreply.github.com>
2021-12-07 20:47:32 -0600

    add image change on mouseover for sock color

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,29 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: '../../assets/images/vue-socks.jpg',
+        image: './assets/images/vue-socks.jpg',
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: '../../assets/images/vue-socks.jpg'
+                variantImage: './assets/images/vue-socks.jpg'
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: '../../assets/images/vue-socks-blue.jpg'
+                variantImage: './assets/images/vue-socks-blue.jpg'
             }
         ],
         cart: 0
     },
     methods: {
-        addToCart: function () {
+        addToCart() {
             this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
         }
     }
 })
